b6c719d9b0fc8c47ba88f0168dda0c434e973c4d
Sara Fishburn <fishburn.sara@gmail.com>
2021-05-19 12:11:34 +1000

    trying to get heroku deployment working

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  root to: "filaments_and_resins#index"
+  root to: "searches#index"
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
