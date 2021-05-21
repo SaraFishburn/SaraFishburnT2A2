7c7c89ace95f593381c438b081c7d6f7552126bc
Sara Fishburn <fishburn.sara@gmail.com>
2021-05-21 15:43:31 +1000

    got cloudinary somewhat working

--- Gemfile
+++ Gemfile
@@ -64,3 +64,5 @@ gem "pundit", "~> 2.1"
 gem "rolify", "~> 6.0"
 
 gem "cloudinary", "~> 1.20"
+
+gem "activestorage-cloudinary-service", "~> 0.2.3"
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,2 +1,7 @@
 class SearchesController < ApplicationController
+  # before_action: change string form address to lomg and lat 
+  def index
+    puts params
+    # logic to search database for nearby 
+  end
 end
